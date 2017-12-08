7e08ed376ad2f0a2ba32495707a1f4a509c84388
jingsss <jings@umich.edu>
2017-12-08 16:19:06 -0500

    py

--- generate_train_test.py
+++ generate_train_test.py
@@ -2,6 +2,7 @@
 
 from pydub import AudioSegment
 import os,shutil
+import numpy as np
 def segment(label_filename, audio_filename, label_cnt, labels, directory):
 	newAudio = AudioSegment.from_wav(audio_filename)
 	newAudio = newAudio.set_channels(1)
@@ -16,11 +17,12 @@ def segment(label_filename, audio_filename, label_cnt, labels, directory):
 			if label not in labels:
 				continue
 			else:
-				cnt = label_cnt.get(label, default)
-				new_name = directory + "/" + label + "_" + str(cnt) + ".wav"
-				label_cnt[label] = cnt + 1
-				tmp = newAudio[t1:t2]
-				tmp.export(new_name, format="wav")
+				if label != 'N' or np.random.uniform(0,1) < 0.15:
+					cnt = label_cnt.get(label, default)
+					new_name = directory + "/" + label + "_" + str(cnt) + ".wav"
+					label_cnt[label] = cnt + 1
+					tmp = newAudio[t1:t2]
+					tmp.export(new_name, format="wav")
 	
 def mkdir(directory):
 	try:
@@ -30,17 +32,19 @@ def mkdir(directory):
 		os.makedirs(directory)
 
 		
-#audio_filename = ["audio/SHREK_2.wav", "audio/cartoons.wav","audio/ice_age.wav"]
-#filename = ['label/SHREK_2.csv','label/cartoons.csv','label/ice_age.csv']	
-#directory = "combine_data/train"
-audio_filename = ["audio/ice_age.wav"]
-filename = ['label/ice_age.csv']	
-directory = "combine_data/test"
+audio_filename = ["audio/SHREK_2.wav", "audio/cartoons.wav","audio/ice_age_a_mammoth_christmas.wav", "audio/team_america.wav"]
+filename = ['label/SHREK_2.csv','label/cartoons.csv','label/ice_age_a_mammoth_christmas.csv','label/team_america.csv']	
+directory = "combine_data/train_resample"
+
+#audio_filename = ["audio/chicken_run.wav", "audio/ice_age.wav"]
+#filename = ['label/chicken_run.csv', 'label/ice_age.csv']	
+#directory = "combine_data/test_resample"
 mkdir(directory)
 
 labels = ["N","J","S","F","A","C","D"]
 label_cnt = dict()
 default = 0
+
 for i in range(len(audio_filename)):
 	segment(filename[i], audio_filename[i], label_cnt, labels, directory)
 	print label_cnt
